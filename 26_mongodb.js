317d19d59b45af2f0690e7613894ec5c8a9e4a5e
ragnar <mystparkkorea@gmail.com>
2022-09-21 22:02:37 +0900

    mongo 연결 후 로그인, 회원 수정 REST 추가

--- 26_mongodb.js
+++ 26_mongodb.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const bodyparser = require('body-parser');
+const bodyparser = require('body-parser'); // post 방식에서 사용
 const MongoCliend = require('mongodb').MongoClient;
 
 const app = express();
-const router = express.Router();
+const router = express.Router(); // 블록화 시켜서 하나의 앱처럼 사용하기위해 라우터 사용
 const port = 3000;
 
 let database; // 몽고디비 연결 객체
@@ -47,11 +47,79 @@ router.route('/member/regist').post((req, res) =>{
     }else{
         res.writeHead('200', {'content-type':'text/html;charset=utf8'});
         res.write('<h2>데이터베이스 연결 실패</h2>');
-        res.write('<p>mongodb 데이터베이스에 연갈하지 못했습니다.</p>');
+        res.write('<p>mongodb 데이터베이스에 연결하지 못했습니다.</p>');
         res.end();
     }
 } )
 
+// 로그인
+// http://127.0.0.1/member/login (post)
+router.route('/member/login').post((req, res) => {
+    console.log('/member/login 호출!');
+    const userid = req.body.userid;
+    const userpw = req.body.userpw;
+    
+    console.log(`userid : ${userid}, userpw: ${userpw}`);
+
+    if(database){
+        loginMember(database, userid, userpw, (err, result) => {
+            if(err){
+                res.writeHead('200', {'content-type':'text/html;charset=utf8'});
+                res.write('<h2>로그인 실패</h2>');
+                res.write('<p>오류가 발생했습니다.</p>');
+                res.end();
+            }else{
+                if(result){
+                    // result가 리스트로 오기 때문에 (find가 여러 개를 조회하기 때문에 toArray 메소드 사용으로 인해) 
+                    const result_userid = result[0].userid; // 중복된 아이디(사용자)가 없다고 가정하고 0 번째 객체에 바로 접근
+                    const result_userpw= result[0].userpw; 
+                    const result_name= result[0].name; 
+                    const result_position= result[0].position; 
+
+                    res.writeHead('200', {'content-type':'text/html;charset=utf8'});
+                    res.write('<h2>로그인 성공</h2>');
+                    res.write(`<p>아이디 ${result_userid}님 환영합니다!</p>`);
+                    res.write(`<p>비밀번호 : ${result_userpw}</p>`);
+                    res.write(`<p>이름 : ${result_name}</p>`);
+                    res.write(`<p>포지션 :${result_position}</p>`);
+                    res.end();
+                }else{
+                    res.writeHead('200', {'content-type':'text/html;charset=utf8'});
+                    res.write('<h2>로그인 실패</h2>');
+                    res.write('<p>아이디 또는 비밀번호를 확인하세요.</p>');
+                    res.end();
+                }
+            }
+        })
+    }else{
+        res.writeHead('200', {'content-type':'text/html;charset=utf8'});
+        res.write('<h2>데이터베이스 연결 실패</h2>');
+        res.write('<p>mongodb 데이터베이스에 연결하지 못했습니다.</p>');
+        res.end();
+    }
+});
+// 정보 수정
+// http://127.0.0.1/member/edit (post)
+router.route('/member/edit').put((req, res) => {
+    console.log('/member/edit 호출!');
+
+    const userid = req.body.userid;
+    const userpw = req.body.userpw;
+    const name = req.body.name;
+    const position = req.body.position;
+
+    console.log(`userid: ${userid}, userpw:${userpw}, name:${name}, position:${position}`)
+    if(database){
+        editMember(database, userid, userpw, name, position, (err, result) => {
+
+        });
+    }else{
+        res.writeHead('200', {'content-type':'text/html;charset=utf8'});
+        res.write('<h2>데이터베이스 연결 실패</h2>');
+        res.write('<p>mongodb 데이터베이스에 연결하지 못했습니다.</p>');
+        res.end();
+    }
+})
 
 
 const joinMember = function(database, userid, userpw, name, gender, callback) {
@@ -72,8 +140,50 @@ const joinMember = function(database, userid, userpw, name, gender, callback) {
             callback(null, result);
         }
     }); // 컬렉션에 데이터 객체를 저장한다. 
+}
 
+const loginMember = function(database, userid, userpw, callback){
+    console.log('loginMember 호출!');
+    const members = database.collection('member');
 
+    // 객체를 찾는 메소드 , mongodb에서 처럼 객체로 찾는 방식이다. 여러개를 찾는 메소드 이므로 리스트로 받는다(toArray)
+    members.find({userid:userid, userpw:userpw}).toArray((err, result) => {
+        if(err){
+            console.log(err); // 찾지 못해서가 아닌 에러이다.
+            callback(err, null)
+            return;
+        }else{
+            if(result.length > 0){
+                console.log('사용자를 찾았습니다.');
+                callback(null, result);
+            }else{
+                console.log('일치하는 사용자가 없습니다.');
+                callback(null, null);
+            }
+        }
+    }); 
+}
+
+const editMember = function(database, userid, userpw, name, position, callback){
+    console.log('editMember 호출!');
+    const members = database.collection('member');
+
+     // 객체를 찾고 $set에서 객체로 수정한다. 수정할 객체는 모든 정보를 넘겨 줘야 한다.
+    members.updateOne({userid:userid}, {$set:{userid:userid, userpw:userpw, name:name, position:position}},(err, result) => {
+        if(err){
+            console.log(err);
+            callback(err, null);
+            return;
+        }else{
+            // 수정된 숫자
+            if(result.modifiedCount > 0){
+                console.log(`사용자 document ${result.modifiedCount}명 수정되었습니다.`);
+            }else{
+                console.log('수정된 document가 없습니다.');
+            }
+            callback(null,result)
+        }
+    });
 }
 
 // mongodb 연결 함수
